c1179a4c95c49e705428c033e321fa7047c5d788
Igor Brizack <igor_brizack@hotmail.com>
2022-06-23 15:21:58 -0300

    requisito 5 concluido

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -11,11 +11,62 @@ class Wallet extends React.Component {
   }
 
   render() {
-    // const { coinsData } = this.props;
+    const { coinsData } = this.props;
     return (
       <div>
         <Header />
-        TrybeWallet
+        <form>
+          <label htmlFor="valueInput">
+            Despesa:
+            <input
+              id="valueInput"
+              name="valueInput"
+              data-testid="value-input"
+              type="text"
+            />
+          </label>
+          <label htmlFor="descriptionInput">
+            Descrição:
+            <input
+              id="descriptionInput"
+              name="descriptionInput"
+              data-testid="description-input"
+              type="text"
+            />
+          </label>
+          <label htmlFor="currencies">
+            Moeda:
+            <select
+              id="currencies"
+              aria-label="moeda"
+            >
+              {coinsData.map((coin) => (
+                <option key={ coin } value={ coin }>
+                  {' '}
+                  {coin}
+                </option>
+              ))}
+            </select>
+          </label>
+          <label htmlFor="method">
+            Pagamento:
+            <select data-testid="method-input" id="method">
+              <option value="Dinheiro">Dinheiro</option>
+              <option value="Cartão de crédito">Cartão de crédito</option>
+              <option value="Cartão de débito">Cartão de débito</option>
+            </select>
+          </label>
+          <label htmlFor="tag">
+            Categoria:
+            <select data-testid="tag-input" id="tag">
+              <option value="Alimentação">Alimentação</option>
+              <option value="Lazer">Lazer</option>
+              <option value="Trabalho">Trabalho</option>
+              <option value="Transporte">Transporte</option>
+              <option value="Saúde">Saúde</option>
+            </select>
+          </label>
+        </form>
       </div>
     );
   }
@@ -27,7 +78,7 @@ const mapStateToProps = (globalState) => ({
 
 Wallet.propTypes = {
   dispatch: propTypes.func.isRequired,
-  // coinsData: propTypes.string.isRequired,
+  coinsData: propTypes.string.isRequired,
 };
 
 export default connect(mapStateToProps)(Wallet);
